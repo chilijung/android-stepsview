17c1e11b7987677bdd9bef5c461fbcc2b79704fd
howardchi <howardchi@fougood.com>
2017-06-13 16:14:18 +0800

    init

---
+++ app/src/main/java/io/canner/androidstepsview/MainActivity.java
@@ -0,0 +1,79 @@
+package io.canner.androidstepsview;
+
+import io.canner.stepsview.StepsView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String[] views = {"View 1", "View 2", "View 3", "View 4", "View 5", "View 6",
+            "View 7", "View 8", "View 9", "View 10", "View 11", "View 12"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView mListView = (ListView) findViewById(R.id.list);
+
+        MyAdapter adapter = new MyAdapter(this, 0);
+        adapter.addAll(views);
+
+        mListView.setAdapter(adapter);
+
+    }
+
+    public static class MyAdapter extends ArrayAdapter<String> {
+
+        private final String[] labels = {"Step 1", "Step 2", "Step 3", "Step 4", "Step 5"};
+
+        public MyAdapter(Context context, int resource) {
+            super(context, resource);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.row, null);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.mLabel.setText(getItem(position));
+
+            holder.mStepsView.setCompletedPosition(position % labels.length)
+                    .setLabels(labels)
+                    .setBarColorIndicator(
+                            getContext().getResources().getColor(R.color.material_blue_grey_800))
+                    .setProgressColorIndicator(getContext().getResources().getColor(R.color.orange))
+                    .setLabelColorIndicator(getContext().getResources().getColor(R.color.orange))
+                    .drawView();
+
+            return convertView;
+        }
+
+        static class ViewHolder {
+
+            TextView mLabel;
+            StepsView mStepsView;
+
+            public ViewHolder(View view) {
+                mLabel = (TextView) view.findViewById(R.id.label);
+                mStepsView = (StepsView) view.findViewById(R.id.stepsView);
+            }
+        }
+    }
+
+}
---
+++ stepsview/src/main/java/io/canner/stepsview/StepsView.java
@@ -0,0 +1,135 @@
+package io.canner.stepsview;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class StepsView extends LinearLayout implements StepsViewIndicator.OnDrawListener {
+
+    private StepsViewIndicator mStepsViewIndicator;
+    private FrameLayout mLabelsLayout;
+    private String[] mLabels;
+    private int mProgressColorIndicator = Color.YELLOW;
+    private int mLabelColorIndicator = Color.BLACK;
+    private int mBarColorIndicator = Color.BLACK;
+    private int mCompletedPosition = 0;
+
+    public StepsView(Context context) {
+        this(context, null);
+    }
+
+    public StepsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public StepsView(Context context, AttributeSet attrs,
+                     int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.widget_steps_view, this);
+        mStepsViewIndicator = (StepsViewIndicator) rootView.findViewById(R.id.steps_indicator_view);
+        mStepsViewIndicator.setDrawListener(this);
+        mLabelsLayout = (FrameLayout) rootView.findViewById(R.id.labels_container);
+    }
+
+    public String[] getLabels() {
+        return mLabels;
+    }
+
+    public StepsView setLabels(String[] labels) {
+        mLabels = labels;
+        mStepsViewIndicator.setStepSize(labels.length);
+        return this;
+    }
+
+    public int getProgressColorIndicator() {
+        return mProgressColorIndicator;
+    }
+
+    public StepsView setProgressColorIndicator(int progressColorIndicator) {
+        mProgressColorIndicator = progressColorIndicator;
+        mStepsViewIndicator.setProgressColor(mProgressColorIndicator);
+        return this;
+    }
+
+    public int getLabelColorIndicator() {
+        return mLabelColorIndicator;
+    }
+
+    public StepsView setLabelColorIndicator(int labelColorIndicator) {
+        mLabelColorIndicator = labelColorIndicator;
+        return this;
+    }
+
+    public int getBarColorIndicator() {
+        return mBarColorIndicator;
+    }
+
+    public StepsView setBarColorIndicator(int barColorIndicator) {
+        mBarColorIndicator = barColorIndicator;
+        mStepsViewIndicator.setBarColor(mBarColorIndicator);
+        return this;
+    }
+
+    public int getCompletedPosition() {
+        return mCompletedPosition;
+    }
+
+    public StepsView setCompletedPosition(int completedPosition) {
+        mCompletedPosition = completedPosition;
+        mStepsViewIndicator.setCompletedPosition(mCompletedPosition);
+        return this;
+    }
+
+    public void drawView() {
+        if (mLabels == null) {
+            throw new IllegalArgumentException("labels must not be null.");
+        }
+
+        if (mCompletedPosition < 0 || mCompletedPosition > mLabels.length - 1) {
+            throw new IndexOutOfBoundsException(String.format("Index : %s, Size : %s", mCompletedPosition, mLabels.length));
+        }
+
+        mStepsViewIndicator.invalidate();
+    }
+
+    @Override
+    public void onReady() {
+        drawLabels();
+    }
+
+    private void drawLabels() {
+        List<Float> indicatorPosition = mStepsViewIndicator.getThumbContainerXPosition();
+
+        if (mLabels != null) {
+            for (int i = 0; i < mLabels.length; i++) {
+                TextView textView = new TextView(getContext());
+                textView.setText(mLabels[i]);
+                textView.setTextColor(mLabelColorIndicator);
+                textView.setX(indicatorPosition.get(i));
+                textView.setLayoutParams(
+                        new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                if (i <= mCompletedPosition) {
+                    textView.setTypeface(null, Typeface.BOLD);
+                }
+
+                mLabelsLayout.addView(textView);
+            }
+        }
+    }
+}
