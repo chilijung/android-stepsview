ccb876b73da82d3845c5c085c19302ea79067e0b
howardchi <howardchi@fougood.com>
2017-06-16 10:35:13 +0800

    improvements

--- app/src/main/java/io/canner/androidstepsview/MainActivity.java
+++ app/src/main/java/io/canner/androidstepsview/MainActivity.java
@@ -1,17 +1,7 @@
 package io.canner.androidstepsview;
 
-import io.canner.stepsview.StepsView;
-
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
