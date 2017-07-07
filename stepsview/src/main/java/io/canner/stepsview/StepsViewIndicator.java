46afc267aa5f892e529fcf8af7a93a4dfe3566f9
LiJung Chi <chilijung@gmail.com>
2017-07-07 10:04:38 +0800

    Update StepsViewIndicator.java

--- stepsview/src/main/java/io/canner/stepsview/StepsViewIndicator.java
+++ stepsview/src/main/java/io/canner/stepsview/StepsViewIndicator.java
@@ -101,19 +101,11 @@ public class StepsViewIndicator extends View {
         int specMode = MeasureSpec.getMode(measureSpec);
         int specSize = MeasureSpec.getSize(measureSpec);
 
-        //1. layout给出了确定的值，比如：100dp
-        //2. layout使用的是match_parent，但父控件的size已经可以确定了，比如设置的是具体的值或者match_parent
         if (specMode == MeasureSpec.EXACTLY) {
-            result = specSize; //建议：result直接使用确定值
-        }
-        //1. layout使用的是wrap_content
-        //2. layout使用的是match_parent,但父控件使用的是确定的值或者wrap_content
-        else if (specMode == MeasureSpec.AT_MOST) {
-            result = Math.min(defaultSize, specSize); //建议：result不能大于specSize
-        }
-        //UNSPECIFIED,没有任何限制，所以可以设置任何大小
-        //多半出现在自定义的父控件的情况下，期望由自控件自行决定大小
-        else {
+            result = specSize;
+        } else if (specMode == MeasureSpec.AT_MOST) {
+            result = Math.min(defaultSize, specSize);
+        } else {
             result = defaultSize;
         }
 
